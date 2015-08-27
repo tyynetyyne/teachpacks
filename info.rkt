e1af2dcc8db3cd75e5ed3bc8826acb04c4208e46
Tiina Partanen <tyynetyyne@gmail.com>
2015-08-27 12:04:01 +0300

    Racket Turtle library source and doc.

---
+++ info.rkt
@@ -0,0 +1,14 @@
+#lang info
+
+(define collection "teachpacks")
+
+(define version 0.5)
+
+(define deps '("base" "htdp-lib"
+               ))
+
+(define build-deps '("scribble-lib"
+                     "scribble-doc"
+                     ))
+
+(define scribblings '(["doc/racket_turtle.scrbl" 'multi-page]))
