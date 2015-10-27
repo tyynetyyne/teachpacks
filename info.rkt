d7158ead7c4b51ea447b81deb0260bfefa937baf
Tiina Partanen <tyynetyyne@gmail.com>
2015-10-27 09:28:44 +0200

    Updated dependencies

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 (define version 0.5)
 
 (define deps '("gui-lib"
-               "base" "htdp-lib"))
+               "base" "htdp-lib" "plot-gui-lib" "plot-lib"))
 
 (define build-deps '("scribble-lib"
                      "scribble-doc"
